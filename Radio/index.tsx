07c0944b3a1c5c3b4f5005a986b0d3a1f13cf13c
Artem Rudenko <gvidon@ottofeller.com>
2020-11-26 14:23:32 +0100

    Explicitly use Boolean()

--- Radio/index.tsx
+++ Radio/index.tsx
@@ -17,7 +17,7 @@ export function Radio(props: {
   value?: any
 }) {
   const [field, meta, form] = useField(props.name)
-  const isInvalid = !!meta.error && meta.touched
+  const isInvalid = Boolean(meta.error && meta.touched)
   const onChange = useOnChangeWithInstantValidation({field, form, onChange: props.onChange})
 
   return <Field
